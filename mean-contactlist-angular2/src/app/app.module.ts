eeda46ff657df53df763b991a72657ed8a707e52
charlie249 <56076410+charlie249@users.noreply.github.com>
2021-01-21 14:47:37 +0000

    minor tweek

--- mean-contactlist-angular2/src/app/app.module.ts
+++ mean-contactlist-angular2/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
 @NgModule({
   declarations: [
     AppComponent,
