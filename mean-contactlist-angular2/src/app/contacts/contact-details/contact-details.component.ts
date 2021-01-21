6878d926282a4eaac4b5a8fd6e20f3fbddc46aad
charlie249 <56076410+charlie249@users.noreply.github.com>
2021-01-21 13:24:16 +0000

    edited contact-details.component.ts funcions

--- mean-contactlist-angular2/src/app/contacts/contact-details/contact-details.component.ts
+++ mean-contactlist-angular2/src/app/contacts/contact-details/contact-details.component.ts
@@ -1,16 +1,44 @@
 import { Component, Input } from '@angular/core';
-import
+import { Contact } from '../contact';
+import { ContactService } from '../contact.service'
+
 
 @Component({
-  selector: 'app-contact-details',
+  selector: 'contact-details',
   templateUrl: './contact-details.component.html',
   styleUrls: ['./contact-details.component.css']
 })
+
 export class ContactDetailsComponent {
+  @Input()
+  contact: Contact;
 
-  constructor() { }
+  @Input()
+  createHandler: Function;
 
-  ngOnInit(): void {
+  @Input()
+  updateHandler: Function;
+
+  @Input()
+  deleteHandler: Function;
+
+  constructor (private contactService: ContactService) {}
+
+  createContact(contact: Contact) {
+    this.contactService.createContact(contact).then((newContact: Contact) => {
+      this.createHandler(newContact);
+    });
   }
 
+  updateContact(contac: Contact): void {
+    this.contactService.updateContact(contact).then((updateContact: Contact) => {
+      this.updateHandler(updateContact);
+    });
+  }
+
+  deleteContact(contacId: String):void {
+    this.contactService.deleteContact(contacId).then((deleteContact: String)=> {
+      this.deleteHandler(deleteContactId)
+    });
+  }
 }
